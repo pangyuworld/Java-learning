3e46bafdec548c4575264a5fc9c1caa609a2fd3f
pangyuworld <a904237539@qq.com>
2019-12-28 22:12:24 +0800

    做了几道题

--- algorithm and data structure/src/main/java/com/leecode/p6/Solution.java
+++ algorithm and data structure/src/main/java/com/leecode/p6/Solution.java
@@ -8,66 +8,41 @@ import org.junit.jupiter.api.Test;
  * @ClassName: Solution
  * @Package com.leecode.p6
  * @description:
- * @date 2019/10/11 17:09
- * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 N 字形排列。
+ * @date 2019/10/11 17:09 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 N 字形排列。
  */
 public class Solution {
-    class Node{
-        char val;
-        Node down;
-        Node rightUp;
-        public Node(char x){
-            this.val=x;
-        }
-        public Node setDown(Node node){
-            this.down=node;
-            return this.down;
-        }
 
-        public Node setRightUp(Node rightUp) {
-            this.rightUp = rightUp;
-            return this.rightUp;
-        }
-    }
     public String convert(String s, int numRows) {
-        char[] chars=s.toCharArray();
-        // 新建一个空的头部
-        Node node=new Node(' ');
-        Node result=node;
-        // 标记是否往下进行
-        boolean nextDown=true;
-        int downCount=1;
-        for (int i=0;i<chars.length;i++){
-            if (nextDown){
-                node=node.setDown(new Node(chars[i]));
-                // 计数器+1
-                downCount++;
-                // 如果计数器超过最大，则置换状态
-                if (downCount>numRows){
-                    nextDown=!nextDown;
-                }
-            }else{
-                node=node.setRightUp(new Node(chars[i]));
-                // 计数器-1
-                downCount--;
-                if (downCount==2){
-                    nextDown=!nextDown;
+        StringBuffer result = new StringBuffer();
+        if (numRows<=1){
+            return s;
+        }
+        // 输出第1行
+        for (int j = 0; (numRows - 1) * 2 * j < s.length(); j++) {
+            int index = (numRows - 1) * 2 * j;
+            result.append(s.charAt(index));
+        }
+        // 输出中间部分
+        for (int i = 1; i < numRows - 1; i++) {
+            for (int j = 0; (numRows - 1) * 2 * j + i < s.length(); j++) {
+                int index = (numRows - 1) * 2 * j + i;
+                result.append(s.charAt(index));
+                index = (numRows - 1) * 2 * (j + 1) - i;
+                if (index < s.length()) {
+                    result.append(s.charAt(index));
                 }
             }
         }
-        return "";
-    }
-
-    public String combination(Node node,int numRows){
-        String[] strings=new String[numRows];
-        int i=0;
-        while(node.down!=null||node.rightUp!=null){
-
+        // 输出最后一行
+        for (int j = 0; (numRows - 1) * 2 * j + numRows - 1 < s.length(); j++) {
+            int index = (numRows - 1) * 2 * j + numRows - 1;
+            result.append(s.charAt(index));
         }
-        return "";
+        return result.toString();
     }
+
     @Test
-    public void test(){
-        convert("LEETCODEISHIRING",4);
+    public void test() {
+        System.out.println(convert("A", 1));
     }
 }
---
+++ algorithm and data structure/src/main/java/com/leecode/p7/Solution.java
@@ -0,0 +1,32 @@
+package com.leecode.p7;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pang
+ * @version V1.0
+ * @ClassName: Solution
+ * @Package com.leecode.p7
+ * @description: 整数反转 https://leetcode-cn.com/problems/reverse-integer/
+ * @date 2019/12/28 21:05
+ */
+class Solution {
+    public int reverse(int x) {
+        long s=x;
+        long result=0;
+        while (s!=0){
+            long d=s%10;
+            result=result*10+d;
+            s/=10;
+        }
+        if (result>Integer.MAX_VALUE||result<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) result;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(this.reverse(1463847412));
+    }
+}
